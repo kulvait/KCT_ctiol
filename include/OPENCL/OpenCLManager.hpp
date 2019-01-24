c69d26c3e4eadef4792a049a1dea588543296b72
kulvait <kulvait@gmail.com>
2019-01-24 10:30:40 +0100

    OpenCLManager static functions

--- include/OPENCL/OpenCLManager.hpp
+++ include/OPENCL/OpenCLManager.hpp
@@ -14,8 +14,8 @@ namespace util {
     class OpenCLManager
     {
     public:
-        std::shared_ptr<cl::Platform> getPlatform(uint32_t id = 0, bool verbose = false);
-        std::shared_ptr<cl::Device> getDevice(const cl::Platform& platform, uint32_t id = 0, bool verbose = false);
+        static std::shared_ptr<cl::Platform> getPlatform(uint32_t id = 0, bool verbose = false);
+        static std::shared_ptr<cl::Device> getDevice(const cl::Platform& platform, uint32_t id = 0, bool verbose = false);
     };
 } // namespace util
 } // namespace CTL
