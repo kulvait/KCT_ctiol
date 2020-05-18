9dd5ca03c20bc90b60b29ed4530ab2b5bdccb68c
kulvait <kulvait@gmail.com>
2020-05-18 14:47:46 +0200

    Formating

--- include/PROG/ArgumentsForce.hpp
+++ include/PROG/ArgumentsForce.hpp
@@ -4,18 +4,17 @@
 #include "PROG/Arguments.hpp"
 
 namespace CTL::util {
-//Virtual inheritance to have only one instance of Arguments in the pedigree
+// Virtual inheritance to have only one instance of Arguments in the pedigree
 class ArgumentsForce : public virtual Arguments
 {
 public:
     ArgumentsForce(int argc, char** argv, std::string prgName)
         : Arguments(argc, argv, prgName){};
 
-	bool force = false;
+    bool force = false;
+
 protected:
     void addForceArgs();
-
 };
 
 } // namespace CTL::util
-
