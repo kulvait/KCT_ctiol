adfa5b1a702ef99d81d1187997a4cbef2e01b4ae
Vojtěch Kulvait <kulvait@gmail.com>
2022-12-15 19:38:30 +0100

    Handle file existence with compatible file
    On force if the file is compatible it is left
    If the file is not compatible it is removed

--- include/PROG/ArgumentsForce.hpp
+++ include/PROG/ArgumentsForce.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "CLI/CLI.hpp" //Command line parser
-#include "rawop.h"
+#include "DEN/DenFileInfo.hpp"
 #include "PROG/Arguments.hpp"
+#include "rawop.h"
 
 namespace KCT::util {
 // Virtual inheritance to have only one instance of Arguments in the pedigree
@@ -16,12 +17,26 @@ public:
      * Standard handling of file existence.
      *
      * @param f File name.
-     * @param force Forca parametter.
+     * @param force Force parametter.
      * @param removeIfExist Remove file f if it exists.
      *
-     * @return   0 if file do not exist or it exists and force is specified.
+     * @return 1 if file exists and force is not specified  0 if file do not exist or it was
+     * removed, -1 if file exists and is kept.
      */
-    const int handleFileExistence(const std::string &f, bool force, bool removeIfExist);
+    const int handleFileExistence(const std::string& f, bool force, bool removeIfExist);
+
+    /**
+     * @brief
+     *
+     * @param f File name.
+     * @param force Force parametter.
+     * @param compatible_inf DenFileInfo of file that shall be compatible
+     *
+     * @return 1 if file exists and force is not specified  0 if file do not exist or it was
+     * removed, -1 if file exists and is kept.
+     */
+    const int
+    handleFileExistence(const std::string& f, bool force, KCT::io::DenFileInfo compatible_inf);
 
     bool force = false;
 
--- src/PROG/ArgumentsForce.cpp
+++ src/PROG/ArgumentsForce.cpp
@@ -13,7 +13,7 @@ void ArgumentsForce::addForceArgs()
                                      force ? "true" : "false")));
 }
 
-const int ArgumentsForce::handleFileExistence(const std::string &f, bool force, bool removeIfExist)
+const int ArgumentsForce::handleFileExistence(const std::string& f, bool force, bool removeIfExist)
 {
     std::string ERR;
     if(io::pathExists(f))
@@ -24,6 +24,50 @@ const int ArgumentsForce::handleFileExistence(const std::string &f, bool force, 
             {
                 LOGI << io::xprintf("Removing existing file %s", f.c_str());
                 std::remove(f.c_str());
+            } else
+            {
+                return -1;
+            }
+        } else
+        {
+            ERR = io::xprintf(
+                "Error: output file %s already exists, use --force to force overwrite.", f.c_str());
+            LOGE << ERR;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+const int ArgumentsForce::handleFileExistence(const std::string& f,
+                                              bool force,
+                                              KCT::io::DenFileInfo compatible_inf)
+{
+    std::string ERR;
+    if(io::pathExists(f))
+    {
+        if(force)
+        {
+            io::DenFileInfo inf(f);
+            std::string compatibleF = compatible_inf.getFileName();
+            if(!inf.isValid())
+            {
+                LOGI << io::xprintf("File %s is not valid DEN file, removing!", f.c_str());
+                std::remove(f.c_str());
+            } else if(inf.getElementType() != compatible_inf.getElementType())
+            {
+                LOGI << io::xprintf("File %s has incompatible file type with %s, removing!",
+                                    f.c_str(), compatibleF.c_str());
+                std::remove(f.c_str());
+            } else if(inf.dimx() != compatible_inf.dimx() || inf.dimy() != compatible_inf.dimy()
+                      || inf.dimz() != compatible_inf.dimz())
+            {
+                LOGI << io::xprintf("File %s has different dimensions than %s, removing!",
+                                    f.c_str(), compatibleF.c_str());
+                std::remove(f.c_str());
+            } else
+            {
+                return -1;
             }
         } else
         {
