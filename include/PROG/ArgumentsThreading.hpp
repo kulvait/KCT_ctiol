f03096a75eb42110bdcbcc99711756f8439b7ff4
Vojtěch Kulvait <kulvait@gmail.com>
2024-07-11 14:07:52 +0200

    ArgumentsThreading: Adjust threading arguments to dynamically use available processors
    - Modified `ArgumentsThreading` class to dynamically determine the number of processors available on the system using `std::thread::hardware_concurrency()`.
    - Updated the default value of threads to be equal to the number of processors.
    - Adjusted the maximum number of threads to be ten times the number of processors.
    - Implemented a callback function for the threads option to handle negative values:
      - Negative values represent fractions of the total number of processors.
      - For example, -1 means use the number of processors, -2 means use half the number of processors, etc.
    - Updated help string to reflect the new functionality of negative values.
    - Ensured that the `threads` value is clamped to a maximum of 10 times the number of processors.
    
    This change improves the flexibility and efficiency of the threading mechanism by automatically adapting to the system's capabilities and allowing intuitive control over thread allocation.

--- include/PROG/ArgumentsThreading.hpp
+++ include/PROG/ArgumentsThreading.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include <thread>
 
 #include "CLI/CLI.hpp" //Command line parser
 #include "PROG/Arguments.hpp"
@@ -8,12 +10,19 @@ class ArgumentsThreading : public virtual Arguments
 {
 public:
     ArgumentsThreading(int argc, char** argv, std::string prgName)
-        : Arguments(argc, argv, prgName){};
+        : Arguments(argc, argv, prgName)
+    {
+        numProcessors = std::thread::hardware_concurrency();
+        threads = numProcessors; // Set the default value to the number of processors
+    }
 
     uint32_t threads = 0;
 
 protected:
     void addThreadingArgs(CLI::Option_group* og = nullptr);
+
+private:
+    uint32_t numProcessors = 0;
 };
 
 } // namespace KCT::util
--- src/PROG/ArgumentsThreading.cpp
+++ src/PROG/ArgumentsThreading.cpp
@@ -6,18 +6,36 @@ using namespace KCT::util;
 void ArgumentsThreading::addThreadingArgs(CLI::Option_group* og)
 {
     CLI::Option* threads_opt;
-	std::string spec = "-j,--threads";
-	std::string help = 
-            io::xprintf("Number of extra threads that the program can use, default is "
-                        "%d. Zero value denotes no threading.",
-                        threads);
+    std::string spec = "-j,--threads";
+    std::string help
+        = io::xprintf("Number of extra threads that the program can use, default is %d. "
+                      "Zero value denotes no threading. Negative values represent fractions "
+                      "of cores, e.g., -1 means use %d cores, -2 means use %d cores, etc.",
+                      threads, numProcessors, numProcessors / 2);
+
+    auto thread_callback = [this](const int32_t& value) {
+        if(value < 0)
+        {
+            threads = numProcessors / -value;
+        } else
+        {
+            threads = value;
+        }
+        if(threads > 10 * numProcessors)
+        {
+            threads = 10 * numProcessors;
+        }
+        LOGI << io::xprintf("Using %d threads", threads);
+    };
+
     if(og == nullptr)
     {
-        threads_opt = cliApp->add_option( spec, threads, help);
-    }else
-	{
-        threads_opt = og->add_option( spec, threads, help);
-	}
-    threads_opt->check(CLI::Range(0, 65535));
-	registerOption("threads", threads_opt);	
+        threads_opt = cliApp->add_option_function<int32_t>(spec, thread_callback, help);
+    } else
+    {
+        threads_opt = og->add_option_function<int32_t>(spec, thread_callback, help);
+    }
+
+    threads_opt->check(CLI::Range(-10, static_cast<int32_t>(numProcessors * 10)));
+    registerOption("threads", threads_opt);
 }
